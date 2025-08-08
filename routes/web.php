6386478f98dfb083d83a4527f9262888b0b08dc4
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-08-09 02:08:16 +0300

    feat: integrate Bootstrap and Sass for improved styling
    - Added Bootstrap and Sass as dependencies in package.json.
    - Updated Vite configuration to compile Sass files.
    - Created a new Sass file for variables and imported Bootstrap styles.
    - Modified the layout and authentication views to utilize Bootstrap components for better UI.
    - Implemented authentication controllers for login, registration, password reset, and email verification.
    - Added routes for authentication and home controller.
    - Created views for login, registration, password confirmation, password reset, and email verification.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,7 @@ Route::get('/', function () {
 Route::get("/contact", function () {
     return view('front.contact');
 })->name('contact');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
