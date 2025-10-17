0f07ae44a35b9525825f3f7fae83435c1a7f44f2
Mohammed-tareq <mohtareq1999m@gmail.com>
2025-10-17 19:24:15 +0300

    Enhance user dashboard functionality: add notification management, post editing capabilities, and user settings updates. Implement new request validations for post updates and user settings, and improve image handling for user profiles and posts.

---
+++ app/Http/Middleware/CheckNotificationRead.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckNotificationRead
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if($request->query('notifiy')){
+            $notification = auth()->user()->unreadNotifications()->where('id' , $request->query('notifiy'))->first();
+          if($notification){
+
+            $notification->markAsRead();
+          }
+        }
+        return $next($request);
+    }
+}
